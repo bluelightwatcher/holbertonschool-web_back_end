da69d2528f0a32c32746d06eb722807920063bc2
bluelightwatcher <9550@holbertonstudents.com>
2024-11-26 12:03:20 +0100

    using filter and map to extract id value

---
+++ ES6_data_manipulation/1-get_list_students_ids.js
@@ -0,0 +1,10 @@
+import getListStudents from "./0-get_list_students.js";
+
+export default function get_list_students_ids() {
+  if (!Array.isArray(students)) {
+    return [];
+ }
+  return students
+    .filter(student => (student.id !== undefined)
+    .map(student => student.id)
+};
