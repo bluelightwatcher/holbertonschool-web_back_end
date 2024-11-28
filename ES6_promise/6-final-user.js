95e8a036856ef00fe1aaf243870f975fe7e4654b
bluelightwatcher <9550@holbertonstudents.com>
2024-11-28 11:29:36 +0100

    Return settled promises as result, return status on success and reason on failure full test passed

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,22 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
+
+  return results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return {
+        status: result.status,
+        value: result.value,
+      };
+    }
+    return {
+      status: result.status,
+      value: result.reason,
+    };
+  });
+}
