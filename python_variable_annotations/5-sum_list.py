1c9a34c81409750f025d829c0290fa547e4c77b6
bluelightwatcher <9550@holbertonstudents.com>
2024-12-06 14:28:34 +0100

    thanks cheat sheet

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Module makes the sum of a list"""
+
+
+def sum_list(input_list: [float]) -> float:
+    """
+    Computes the sum of a list of floats.
+
+    Args:
+        input_list (List[Float]): A list of floats.
+
+    Returns:
+        float: The sum of the floats in the list.
+    """
+    return sum(input_list)
