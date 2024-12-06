11481a48c6db46b39a84dfb6fb31a7bb7a6c5a34
bluelightwatcher <9550@holbertonstudents.com>
2024-12-06 14:02:46 +0100

    Adding pycode style and some documentation

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
-import typing 
+import typing
 """Contains to_str func"""
 
 
 def to_str(n: float) -> str:
-    """takes a float and return it as string"""
+    """takes a float and return it as string
+    Arg:
+        n (float)
+
+    Returns:
+        string: n
+    """
     return str(n)
